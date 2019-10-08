f403e5b318d100604946cc9b2cf9216fb03ebbaa
aleksey rudometov <iambeone@gmail.com>
2019-10-08 16:57:41 +0500

    sources update

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-require("dotenv").config();
-
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./lib/schema");
 const resolvers = require("./lib/resolvers");
 const CosmosAPI = require("./lib/cosmos-source");
-const networkData = require("./data/networks");
+const GaiaAPI = require("./lib/gaia-source");
+const { networks } = require("./data/networks");
+const config = require("./config");
+
+const cosmosApi = new CosmosAPI(networks["cosmoshub"]);
+const gaiaApi = new GaiaAPI(networks["gaia-testnet"]);
 
 let options = {
   typeDefs,
   resolvers,
   dataSources: () => ({
-    cosmosAPI: new CosmosAPI(process.env.CHAIN_URL),
-    networkData
+    CosmosAPI: cosmosApi,
+    GaiaAPI: gaiaApi,
+    networks
   }),
   cacheControl: {
     defaultMaxAge: 5000
@@ -19,19 +23,19 @@ let options = {
   introspection: true,
   playground: true,
   engine: {
-    apiKey: process.env.ENGINE_API_KEY,
+    apiKey: config.apollo_engine_api_key,
     schemaTag: "production"
   }
 };
 
-if (process.env.ENABLE_CACHE) {
+if (config.enable_cache) {
   options.cache = require("./lib/redis").createCache();
 }
 
 const server = new ApolloServer(options);
 
 server
-  .listen({ port: process.env.PORT || 4000 })
+  .listen({ port: config.port })
   .then(({ url, subscriptionsUrl }) => {
     console.log(`Server ready at ${url}`);
     console.log(`Subscriptions ready at ${subscriptionsUrl}`);
